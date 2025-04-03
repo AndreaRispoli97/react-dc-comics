34e9742cc4a376653b7f327e6559d1055f431303
AndreaRispoli97 <Andrea9715@outlook.it>
2025-04-03 16:10:35 +0200

    add: inserimento logo e prime funzioni

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-
+import Header from "./components/Header.jsx"
+import Main from "./components/Main"
+import Footer from "./components/Footer.jsx"
+// import Logo from "./components/Logo"
 
 function App() {
 
 
   return (
     <>
+      <Header />
+      <Main />
+      <Footer />
 
     </>
   )
