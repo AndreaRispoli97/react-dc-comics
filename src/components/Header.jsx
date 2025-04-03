db6d3ce74e213110ea219fbaec4baa36bd88e218
AndreaRispoli97 <Andrea9715@outlook.it>
2025-04-03 16:35:22 +0200

    add: inserimento header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,23 @@ import Logo from "./Logo.jsx"
 function Header() {
     return (
         <>
-            <Logo />
+            <header>
+                <div><Logo /></div>
+                <div>
+                    <ul>
+                        <li>CHARACTERS</li>
+                        <li>COMICS</li>
+                        <li>MOVIES</li>
+                        <li>TV</li>
+                        <li>GAMES</li>
+                        <li>COLLECTIBLES</li>
+                        <li>VIDEOS</li>
+                        <li>FANS</li>
+                        <li>NEWS</li>
+                        <li>SHOP</li>
+                    </ul>
+                </div>
+            </header>
         </>
     )
 }
